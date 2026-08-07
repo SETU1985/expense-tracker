af17dbb5b8e4cf03cd695c456ed622e531a9f445
Setu Choudhary <paramparamens@gmail.com>
2026-08-07 10:51:54 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='expense-v14';
+const C='expense-v15';
 self.addEventListener('install',e=>{self.skipWaiting();
   e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']).catch(()=>{})));});
 self.addEventListener('activate',e=>{e.waitUntil(
